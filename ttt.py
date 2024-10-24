cd3a5e908c6a0441e7ee2eda22f43b7b1d7b65a0
Sanjeev Yadav <151318219+sanjeevyadav1111@users.noreply.github.com>
2024-10-24 15:35:45 +0530

    Add files via upload

---
+++ ttt.py
@@ -0,0 +1,32 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.current_player = 'X'  # X starts first
+
+    def print_board(self):
+        for row in self.board:
+            print('|'.join(row))
+            print('-' * 5)
+
+    def is_winner(self, player):
+        # Check rows, columns, and diagonals
+        for i in range(3):
+            if all(self.board[i][j] == player for j in range(3)) or \
+               all(self.board[j][i] == player for j in range(3)):
+                return True
+        if all(self.board[i][i] == player for i in range(3)) or \
+           all(self.board[i][2 - i] == player for i in range(3)):
+            return True
+        return False
+
+    def is_full(self):
+        return all(self.board[i][j] != ' ' for i in range(3) for j in range(3))
+
+    def make_move(self, row, col):
+        if self.board[row][col] == ' ':
+            self.board[row][col] = self.current_player
+            return True
+        return False
+
+    def switch_player(self):
+        self.current_player = 'O' if self.current_player == 'X' else 'X'
